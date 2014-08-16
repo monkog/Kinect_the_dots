a1939e111671d743dfde8fec0c7e152ebf6f161c
monkog <mkmonikakogut@gmail.com>
2014-08-16 09:56:48 +0200

    Debugging Skeleton and a pencil
    Added drawing a skeleton to help debugging and a pencil cursor

--- Kinect_the_dots/Kinect_the_dots/MainWindow.xaml.cs
+++ Kinect_the_dots/Kinect_the_dots/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,240 @@ namespace Kinect_the_dots
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Variables
+        /// <summary>
+        /// Current KinectSensor
+        /// </summary>
+        private KinectSensor m_kinectSensor;
+        /// <summary>
+        /// WritableBitmap that source from Kinect camera is written to
+        /// </summary>
+        private WriteableBitmap m_cameraSourceBitmap;
+        /// <summary>
+        /// Bounds of camera source
+        /// </summary>
+        private Int32Rect m_cameraSourceBounds;
+        /// <summary>
+        /// Number of bytes per line
+        /// </summary>
+        private int m_colorStride;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Current KinectSensor
+        /// </summary>
+        public KinectSensor Kinect
+        {
+            get { return m_kinectSensor; }
+            set
+            {
+                if (m_kinectSensor != value)
+                {
+                    if (m_kinectSensor != null)
+                    {
+                        UninitializeKinectSensor(m_kinectSensor);
+                        m_kinectSensor = null;
+                    }
+                    if (value != null && value.Status == KinectStatus.Connected)
+                    {
+                        m_kinectSensor = value;
+                        InitializeKinectSensor(m_kinectSensor);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += DiscoverKinectSensors;
+            Unloaded += (sender, e) => { Kinect = null; };
         }
+
+        /// <summary>
+        /// Enables ColorStream from newly detected KinectSensor and sets output image
+        /// </summary>
+        /// <param name="sensor">Detected KinectSensor</param>
+        private void InitializeKinectSensor(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                ColorImageStream colorStream = sensor.ColorStream;
+                colorStream.Enable();
+
+                m_cameraSourceBitmap = new WriteableBitmap(colorStream.FrameWidth, colorStream.FrameHeight
+                    , 96, 96, PixelFormats.Bgr32, null);
+                m_cameraSourceBounds = new Int32Rect(0, 0, colorStream.FrameWidth, colorStream.FrameHeight);
+                m_colorStride = colorStream.FrameWidth * colorStream.FrameBytesPerPixel;
+                KinectCameraImage.Source = m_cameraSourceBitmap;
+
+                sensor.ColorFrameReady += KinectSensor_ColorFrameReady;
+
+                sensor.SkeletonStream.Enable();
+                m_skeletons = new Skeleton[sensor.SkeletonStream.FrameSkeletonArrayLength];
+                sensor.SkeletonFrameReady += KinectSensor_SkeletonFrameReady;
+                sensor.Start();
+            }
+        }
+
+        /// <summary>
+        /// Disables ColorStream from disconnected KinectSensor
+        /// </summary>
+        /// <param name="sensor">Disconnected KinectSensor</param>
+        private void UninitializeKinectSensor(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                sensor.Stop();
+                sensor.ColorFrameReady -= KinectSensor_ColorFrameReady;
+                sensor.SkeletonFrameReady -= KinectSensor_SkeletonFrameReady;
+                sensor.SkeletonStream.Disable();
+                m_skeletons = null;
+            }
+        }
+
+        /// <summary>
+        /// Handles ColorFrameReady event
+        /// </summary>
+        /// <remarks>
+        /// Views image from the camera in KinectCameraImage
+        /// </remarks>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Arguments containing ImageFrame</param>
+        private void KinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    byte[] pixels = new byte[colorFrame.PixelDataLength];
+                    colorFrame.CopyPixelDataTo(pixels);
+
+                    m_cameraSourceBitmap.WritePixels(m_cameraSourceBounds, pixels, m_colorStride, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the primary skeleton
+        /// </summary>
+        /// <param name="m_skeletons">Array of discovered skeletons</param>
+        /// <returns>Primary skeleton</returns>
+        private Skeleton GetPrimarySkeleton(Skeleton[] skeletons)
+        {
+            Skeleton skeleton = null;
+
+            if (skeletons != null)
+            {
+                // Find the skeleton closest to Kinect
+                for (int i = 0; i < skeletons.Length; i++)
+                    if (skeletons[i].TrackingState == SkeletonTrackingState.Tracked)
+                    {
+                        if (skeleton == null)
+                            skeleton = skeletons[i];
+                        else if (skeleton.Position.Z > skeletons[i].Position.Z)
+                            skeleton = skeletons[i];
+                    }
+            }
+
+            return skeleton;
+        }
+
+        /// <summary>
+        /// Gets the primary hand
+        /// </summary>
+        /// <param name="skeleton">Primary skeleton</param>
+        /// <returns>Hand closest to Kinect</returns>
+        private Joint GetPrimaryHand(Skeleton skeleton)
+        {
+            Joint hand = new Joint();
+
+            if (skeleton != null)
+            {
+                hand = skeleton.Joints[JointType.HandLeft];
+                Joint rightHand = skeleton.Joints[JointType.HandRight];
+
+                if (rightHand.TrackingState != JointTrackingState.NotTracked)
+                    if (hand.TrackingState == JointTrackingState.NotTracked
+                        || hand.Position.Z > rightHand.Position.Z)
+                        hand = rightHand;
+            }
+
+            return hand;
+        }
+
+        /// <summary>
+        /// Keeps track of the hand
+        /// </summary>
+        /// <param name="hand">Primary hand</param>
+        private void TrackHand(Joint hand)
+        {
+            if (hand.TrackingState == JointTrackingState.NotTracked)
+            {
+                HandCursor.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            HandCursor.Visibility = Visibility.Visible;
+            
+            DepthImagePoint point = Kinect.CoordinateMapper.MapSkeletonPointToDepthPoint(hand.Position
+                , Kinect.DepthStream.Format);
+            int x = (int)((point.X * HandCanvas.Width / Kinect.DepthStream.FrameWidth)
+                - (HandCursor.ActualWidth / 2.0));
+            int y = (int)((point.Y * HandCanvas.ActualHeight / Kinect.DepthStream.FrameHeight)
+                - (HandCursor.Height / 2.0));
+
+            Canvas.SetLeft(HandCursor, x);
+            Canvas.SetTop(HandCursor, y);
+
+            if (hand.JointType == JointType.HandRight)
+                CursorScale.ScaleX = -1;
+            else
+                CursorScale.ScaleX = 1;
+        }
+
+        /// <summary>
+        /// Subscribes for StatusChanged event and initializes KinectSensor
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Arguments</param>
+        private void DiscoverKinectSensors(object sender, RoutedEventArgs e)
+        {
+            KinectSensor.KinectSensors.StatusChanged += KinectSensor_StatusChanged;
+            Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+        }
+
+        /// <summary>
+        /// Updates KinectSensor
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">Arguments</param>
+        private void KinectSensor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case KinectStatus.Initializing:
+                case KinectStatus.Connected:
+                    if (Kinect == null)
+                        Kinect = e.Sensor;
+                    break;
+                case KinectStatus.Disconnected:
+                    if (Kinect == e.Sensor)
+                    {
+                        Kinect = null;
+                        Kinect = KinectSensor.KinectSensors.FirstOrDefault(x => x.Status == KinectStatus.Connected);
+                        if (Kinect == null)
+                            MessageBox.Show("No Kinect is connected. Please provide one.");
+                    }
+                    break;
+                default:
+                    MessageBox.Show("No Kinect is connected. Please provide one.");
+                    break;
+            }
+        }
+        #endregion
     }
 }
--- Kinect_the_dots/Kinect_the_dots/Properties/Resources.Designer.cs
+++ Kinect_the_dots/Kinect_the_dots/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kinect_the_dots.Properties
-{
-
-
+namespace Kinect_the_dots.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,51 @@ namespace Kinect_the_dots.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kinect_the_dots.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap HandCursor {
+            get {
+                object obj = ResourceManager.GetObject("HandCursor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
